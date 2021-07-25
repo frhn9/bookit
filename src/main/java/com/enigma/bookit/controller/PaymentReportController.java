7258e2438017eb1d085e09a218ca4a973cf83e1e
Shaffan.Haqi <shaffan.haqi@gmail.com>
2021-07-25 08:54:43 +0700

    [FEATURE] added report generator using Jasper

--- src/main/java/com/enigma/bookit/controller/PaymentReportController.java
+++ src/main/java/com/enigma/bookit/controller/PaymentReportController.java
@@ -1,2 +1,19 @@
-package com.enigma.bookit.controller;public class PaymentReportController {
+package com.enigma.bookit.controller;
+
+import com.enigma.bookit.service.PaymentReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gen")
+public class PaymentReportController {
+    @Autowired
+    private PaymentReportService paymentReportService;
+
+    @GetMapping("/report")
+    public String generateReport(){
+        return paymentReportService.generateReport();
+    }
 }
--- src/main/java/com/enigma/bookit/service/PaymentReportService.java
+++ src/main/java/com/enigma/bookit/service/PaymentReportService.java
@@ -1,2 +1,37 @@
-package com.enigma.bookit.service;public class PaymentReportService {
+package com.enigma.bookit.service;
+
+import com.enigma.bookit.entity.Payment;
+import com.enigma.bookit.repository.PaymentRepository;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class PaymentReportService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    public String generateReport(){
+        try{
+            List<Payment> payments = paymentRepository.findAll();
+            String reportPath = "C:\\Users\\Argast\\IdeaProjects\\bookit\\src\\main\\resources";
+            JasperReport jasperReport = JasperCompileManager
+                    .compileReport(reportPath + "\\payment-report.jrxml");
+            JRBeanCollectionDataSource jrBeanCollectionDataSource = new JRBeanCollectionDataSource(payments);
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("createdBy", "Our sadness and sorrow");
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, jrBeanCollectionDataSource);
+            JasperExportManager.exportReportToPdfFile(jasperPrint, reportPath + "\\payment.pdf");
+            return"SO FKIN DONE";
+        }catch(Exception e){
+            e.printStackTrace();
+            return "WHYYYYYYYYYYYYY";
+        }
+    }
 }
